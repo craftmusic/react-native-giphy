06bf0aacabfbde0fdbb8e8d4c32c87770c619a86
Craft Music <49925400+craftmusic@users.noreply.github.com>
2020-04-30 22:07:23 -0400

    Delete RNGiphyKeyboard.m

--- ios/RNGiphyKeyboard.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTEventEmitter.h>
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(RNGiphyKeyboard, RCTEventEmitter)
-
-RCT_EXTERN_METHOD(openGiphy: (NSDictionary*)options)
-RCT_EXTERN_METHOD(dismissGiphy)
-
-@end
